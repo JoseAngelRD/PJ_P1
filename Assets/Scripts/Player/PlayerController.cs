9c47681195f4948e2daee994431b94fb526857a6
Joaqu1nMS <joaquin.martin.ms@gmail.com>
2026-03-25 12:19:21 +0100

    Menu y primeras animaciones player

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -6,11 +7,13 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float velocidad = 5.0f;
+    [SerializeField] private float jumpForce = 150f;
     private Rigidbody2D rb2D;
     private Vector2 movimiento;
     private Animator animator;
 
     // Variables de salto
+    [SerializeField] private LayerMask groundLayer;
     public bool onGround = false;
 
     // Variables de combate
@@ -34,6 +37,12 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Estado del player
+        Debug.DrawRay(transform.position, Vector3.down * 0.4f, Color.red);
+        RaycastHit2D colisionDown = Physics2D.Raycast(transform.position, Vector3.down, 0.4f, groundLayer);
+        if (colisionDown != false && colisionDown.collider.CompareTag("Suelo")) onGround = true;
+        else onGround = false;
+
         /*if (daniado)
         {
             movimiento = Vector2.zero;
@@ -45,18 +54,21 @@ public class PlayerController : MonoBehaviour
             hitsAtaques[animator.GetInteger("Direccion")].SetActive(true);
             return;
         }*/
-
+        
         movimiento.x = Input.GetAxisRaw("Horizontal");
         movimiento = movimiento.normalized;
+
+        if (movimiento.x < 0.0f) transform.localScale = new Vector3 (-Math.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+        else if (movimiento.x > 0.0f) transform.localScale = new Vector3 (Math.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);        
                 
         animator.SetFloat("Horizontal", movimiento.x);        
         animator.SetFloat("Speed", movimiento.magnitude);
 
-        /*if (Input.GetMouseButtonDown(0)) {            
+        if (Input.GetMouseButtonDown(0)) {            
             Debug.Log("LeftClick");
             animator.SetTrigger("Attack");
             atacando = true;            
-        }*/ 
+        }
 
         if (Input.GetKey(KeyCode.A))
         {
@@ -66,9 +78,11 @@ public class PlayerController : MonoBehaviour
             animator.SetInteger("Direccion", 1);
         }
 
-        if ((Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.W)) && onGround)
+        if ((Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.W)) && onGround && rb2D.velocity.y < 0.1f)
         {            
-            rb2D.AddForce(new Vector2(0, 1)*500);
+            rb2D.AddForce(Vector2.up*jumpForce, ForceMode2D.Impulse);
+            animator.SetTrigger("Jump");            
+            StartCoroutine(Aterrizaje());            
         }
     }
 
@@ -78,25 +92,19 @@ public class PlayerController : MonoBehaviour
         {
             return;
         }
-        rb2D.velocity = movimiento * velocidad;
-    }    
+        rb2D.velocity = new Vector2(movimiento.x * velocidad, rb2D.velocity.y);
+    }  
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private IEnumerator Aterrizaje()
     {
-        if (collision.collider.CompareTag("Suelo") && !onGround)
-        {
-            onGround = true;
-            animator.SetTrigger("OnGround");
-        }
-    }
-
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.collider.CompareTag("Suelo"))
-        {
-            onGround = false;
-            animator.SetTrigger("Jump");
-        }
+        // Espera a salir del suelo
+        yield return new WaitUntil(() => !onGround);
+        //Debug.Log("Salgo del suelo");
+        
+        // Esperar a que aterrize
+        yield return new WaitUntil(() => onGround && Mathf.Abs(rb2D.velocity.y) < 0.01f);
+        //Debug.Log("Aterrizo");
+        animator.SetTrigger("OnGround");
     }
 
     public void EndAttack()
